2772d730311e71ec7a9f405cb48774d520ced456
Soumipal56 <soumiisc2020@gmail.com>
2026-05-16 22:43:06 +0530

    fix: express v5 wildcard route for spa

--- oops-app/server/index.js
+++ oops-app/server/index.js
@@ -32,7 +32,7 @@ app.use('/auth', authRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch-all route to serve the frontend (SPA support)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
